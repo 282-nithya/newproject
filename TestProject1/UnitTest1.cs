bb9efa6f2e2de3e8c1b5c556df1147e6c23eb5cc
ETG1 <ETG1@48N2LG2-SHEL>
2022-08-24 17:49:08 +0530

    Add project files.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+using newproject;
+namespace TestProject1
+
+{
+    [TestClass]
+    public class Testcalculator
+    {
+        [TestMethod]
+        public   void TestAdd()
+        {
+            Calculator c = new Calculator();
+            int result = c.Add(10, 20);
+            Assert.AreEqual(30, result);
+
+        }
+    }
+}
